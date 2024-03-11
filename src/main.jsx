e8a07961a1efa4cee6fb7bdfbdc575da6252aa95
iamrajivsinhaa <rajivsinhaajit@gmail.com>
2024-03-11 16:09:38 +0530

    Hi this is latest version with some inline styling

--- src/main.jsx
+++ src/main.jsx
@@ -2,10 +2,31 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+const Day = new Date();
+const Hours = Day.getHours();
+let text;
+const customStyle = {
+    color: ""
+}
+if(Hours<12){
+    text = "Good Morning";
+    customStyle.color = "red";
+}
+else if(Hours>12 && Hours<18){
+    text = " Good Evening";
+    customStyle.color= "green";
+}
+else{
+    text = "Good Night";
+    customStyle.color= "blue";
+};
+
+
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+<>
+<h1 style={customStyle}>Hey {text}</h1>
+
+</>
 )
  
